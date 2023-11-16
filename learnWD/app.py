a3d937dcf05338233da85cfbd83a26ffb7385087
Shifa Khousain <119953244+shifakhousain@users.noreply.github.com>
2023-11-16 07:18:22 -0800

    app.py

--- learnWD/app.py
+++ learnWD/app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def day1():
 	return render_template('day1.html')
+	
+@app.route('/day2')
+def day2():
+	return render_template('day2.html')
+
+@app.route('/day3')
+def day3():
+	return render_template('day3.html')
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
